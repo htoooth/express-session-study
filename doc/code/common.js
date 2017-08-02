5c4256428fddb9791277af16657951a6567786dd
htoooth <ht.anglenx@gmail.com>
2017-08-02 16:35:45 +0800

    fix

---
+++ doc/code/common.js
@@ -0,0 +1,47 @@
+const config = global.yoho.config;
+const session = require('yoho-express-session');
+const onHeaders = require('on-headers');
+const _ = require('lodash');
+
+/**
+ * 该中间件主要把 express-session 和 client-session 集中起来处理，如果 memcached 出错了，使用 cookie session
+ * @param backSession cookeSession 的键名
+ * @returns {function(*=, *=, *)}
+ */
+module.exports = (backSession) => {
+    return (req, res, next) => {
+        let notUseMemcached = _.get(req.app.locals.pc, 'session.removeMemcached', false);
+
+        if (req.session && !notUseMemcached) {
+            req.memcachedSessionError = false;
+        } else {
+            // 重建 session
+            res.emit('sessionError');
+            req.memcachedSessionError = true;
+            req.session = new session.Session(req);
+            req.session.cookie = new session.Cookie({
+                domain: config.cookieDomain,
+                httpOnly: false
+            });
+
+            req.session = Object.assign(req.session, req[backSession].sessionBack);
+        }
+
+        Object.defineProperty(req.session, 'reset', {
+            configurable: true,
+            enumerable: false,
+            value: function() {
+                req.session = null;
+                req[backSession].reset();
+            },
+            writable: false
+        });
+
+        onHeaders(res, function() {
+            // 备份数据
+            req[backSession].sessionBack = req.session;
+        });
+
+        next();
+    };
+};
